b421a5f9fe224c202423c891728f76301a303561
Matt <1009003+tantaman@users.noreply.github.com>
2023-08-17 12:36:50 -0400

    add vlcn logo

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,18 @@ import "./App.css";
 import { useDB, useQuery2 } from "@vlcn.io/react";
 import { AppSchema } from "./schemas/main";
 import TableTable from "./TableTable";
+import vlcnLogo from "./assets/vlcn.png";
 
 function App({ dbname }: { dbname: string }) {
   const ctx = useDB(dbname);
 
   const rows = useQuery2(
     ctx,
-    AppSchema.sql<{/*Could not find the referenced schema typescript type! Is it defined? /Users/tantaman/workspace2/vite-project/src/App.tsx:12:4*/}>`SELECT * FROM test`,
+    AppSchema.sql<{
+      id: number;
+      name: string | null;
+      wing: number | null;
+    }>`SELECT * FROM test`,
     []
   ).data;
 
@@ -23,8 +28,11 @@ function App({ dbname }: { dbname: string }) {
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
+        <a href="https://vlcn.io" target="_blank">
+          <img src={vlcnLogo} className="logo vlcn" alt="Vulcan logo" />
+        </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vite + React + Vulcan</h1>
       <div className="card">
         <TableTable ctx={ctx} tableName="test">
           {rows.map((row) => (
--- src/schemas/main.ts
+++ src/schemas/main.ts
@@ -4,12 +4,12 @@ export const AppSchema = schema<{
   readonly test: Readonly<{
     id: number;
     name: string | null;
-    thing: number | null
+    wing: number | null
   }>
 }>`
 CREATE TABLE IF NOT EXISTS test (
   id INTEGER PRIMARY KEY NOT NULL,
   name TEXT,
-  thing FLOAT
+  wing FLOAT
 );
 `;
