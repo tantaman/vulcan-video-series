86e18e5905275b6312791eba56fd7bf21c4a0ded
Matt <1009003+tantaman@users.noreply.github.com>
2023-08-17 12:33:47 -0400

    latest packages

--- src/schemas/main.ts
+++ src/schemas/main.ts
@@ -3,13 +3,13 @@ import { schema } from "@vlcn.io/typed-sql";
 export const AppSchema = schema<{
   readonly test: Readonly<{
     id: number;
-    name: string;
-    thing: number | null;
-  }>;
+    name: string | null;
+    thing: number | null
+  }>
 }>`
 CREATE TABLE IF NOT EXISTS test (
   id INTEGER PRIMARY KEY NOT NULL,
-  name TEXT NOT NULL,
+  name TEXT,
   thing FLOAT
 );
 `;
